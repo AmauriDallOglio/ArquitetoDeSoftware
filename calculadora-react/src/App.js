393840a3d280ec8833331529805d35649ede91a4
Amauri Dall'Oglio <amauridalloglio@outlook.com.br>
2024-08-06 20:28:08 -0300

    Criando os Botões Principais; Desenvolvendo Interações nos Botões;

--- calculadora-react/src/App.js
+++ calculadora-react/src/App.js
@@ -1,11 +1,38 @@
 
-import { Container, Content } from './styles';
+import Input from './components/Input';
+import Button from './components/Button';
+import { Container, Content, Row, Column } from './styles';
 
 const App = () => {
   return (
     <Container>
       <Content>
         <h1>Calculadora React</h1>
+        <p>npx create-react-app calculadora-react </p>
+        <p>cd calculadora-react </p>
+        <p>npm install styled-components </p>
+        <p>yarn add styled-components </p>
+        <p>npm start </p>
+        <Input> </Input>
+        <Row>
+          <Button label="7"/>  
+          <Button label="8"/> 
+          <Button label="9"/> 
+          <Button label="-"/> 
+        </Row>
+        <Row>
+          <Button label="4"/>  
+          <Button label="5"/> 
+          <Button label="6"/> 
+          <Button label="+"/> 
+        </Row>
+        <Row>
+          <Button label="1"/>  
+          <Button label="2"/> 
+          <Button label="3"/> 
+          <Button label="="/> 
+        </Row>
+         
       </Content>
     </Container>
   );
--- calculadora-react/src/components/Button/styles.js
+++ calculadora-react/src/components/Button/styles.js
@@ -1,1 +1,12 @@
 import styled from 'styled-components';
+
+export const ButtonContainer = styled.button`
+    padding: 10px;
+    border: 1px solid #Cdcdcd;
+    background-color: #0000FF;
+    color: #FFFFFF;
+    font-size: 24px;
+    font-jeight:700;
+    flex: 1;
+
+`
