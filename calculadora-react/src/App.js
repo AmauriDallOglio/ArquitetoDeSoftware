5747e51c5eb09184ec28e9b572fc5251c3b792eb
Amauri Dall'Oglio <amauridalloglio@outlook.com.br>
2024-08-07 22:44:22 -0300

    Função de Soma e Igual e subtração

--- calculadora-react/src/App.js
+++ calculadora-react/src/App.js
@@ -2,8 +2,70 @@
 import Input from './components/Input';
 import Button from './components/Button';
 import { Container, Content, Row, Column } from './styles';
+import { useState } from 'react';
+
+
 
 const App = () => {
+  const [currentNumber, setCurrentNumber] = useState(0);
+  const [firstNumber, setFirstNumber] = useState('0');
+  const [operation, SetOpration] = useState('');
+
+  const handleNumber = (num) => {
+    setCurrentNumber(prev => `${prev == '0' ? '' : prev}${num}`)
+  };
+  const handOnClear = () => {
+    setCurrentNumber('0')
+    setFirstNumber('0')
+    SetOpration('')
+  };
+  const handlerSumNumbers = () => {
+    if(firstNumber == '0'){
+        setFirstNumber(String(currentNumber));
+        setCurrentNumber('0')
+        SetOpration('+')
+    }else{
+      const sum = Number(firstNumber) + Number(currentNumber);
+      setCurrentNumber(String(sum))
+      SetOpration('')
+    }
+  };
+
+  const handlerMinusNumbers = () => {
+    if(firstNumber == '0'){
+        setFirstNumber(String(currentNumber));
+        setCurrentNumber('0')
+        SetOpration('-')
+    }else{
+      const minus = Number(firstNumber) - Number(currentNumber);
+      setCurrentNumber(String(minus))
+      SetOpration('')
+    }
+  };
+
+  const handlerEquals = () => {
+    if(!firstNumber !== '0' && operation !=='' && currentNumber !== '0'){
+      switch(operation){
+        case '+':
+          handlerSumNumbers();
+          break
+        case '-':
+          handlerMinusNumbers();
+          break
+        default:
+          break;
+      }
+      
+      setFirstNumber(String(currentNumber));
+        setCurrentNumber('0')
+    } 
+  };
+
+
+
+
+
+
   return (
     <Container>
       <Content>
@@ -13,24 +75,30 @@ const App = () => {
         <p>npm install styled-components </p>
         <p>yarn add styled-components </p>
         <p>npm start </p>
-        <Input> </Input>
+        <Input value={currentNumber}/>  
         <Row>
-          <Button label="7"/>  
-          <Button label="8"/> 
-          <Button label="9"/> 
-          <Button label="-"/> 
+          <Button label="x" onClick={() => handleNumber('')}/>  
+          <Button label="/" onClick={() => handleNumber('')}/> 
+          <Button label="C" onClick={handOnClear}/> 
+          <Button label="X" onClick={() => handleNumber('')}/> 
         </Row>
         <Row>
-          <Button label="4"/>  
-          <Button label="5"/> 
-          <Button label="6"/> 
-          <Button label="+"/> 
+          <Button label="7" onClick={() => handleNumber('7')}/>  
+          <Button label="8" onClick={() => handleNumber('8')}/> 
+          <Button label="9" onClick={() => handleNumber('9')}/> 
+          <Button label="-" onClick={handlerMinusNumbers}/> 
         </Row>
         <Row>
-          <Button label="1"/>  
-          <Button label="2"/> 
-          <Button label="3"/> 
-          <Button label="="/> 
+          <Button label="4" onClick={() => handleNumber('4')}/>  
+          <Button label="5" onClick={() => handleNumber('5')}/> 
+          <Button label="6" onClick={() => handleNumber('6')}/> 
+          <Button label="+" onClick={handlerSumNumbers}/> 
+        </Row>
+        <Row>
+          <Button label="1" onClick={() => handleNumber('1')}/>  
+          <Button label="2" onClick={() => handleNumber('2')}/> 
+          <Button label="3" onClick={() => handleNumber('3')}/> 
+          <Button label="=" onClick={handlerEquals}/> 
         </Row>
          
       </Content>
--- calculadora-react/src/components/Button/styles.js
+++ calculadora-react/src/components/Button/styles.js
@@ -9,4 +9,7 @@ export const ButtonContainer = styled.button`
     font-jeight:700;
     flex: 1;
 
+    &:hover{
+        opacity: 0.6;
+    }
 `
